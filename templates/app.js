2ae328b391c50d26d56350b018b08b87e845e360
Tanvi Somvanshi <24somta@goodnews.vic.edu.au>
2024-07-30 06:58:06 +1000

    attempted to input image and create a login page.

--- templates/app.js
+++ templates/app.js
@@ -1,7 +1,23 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
+const getStartedBtn = document.querySelector('#get-started-btn');
+const signInBtn = document.querySelector('.nav-links-btn');
+const overlay = document.querySelector('#overlay');
+const closeBtn = document.querySelector('#close-btn');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+getStartedBtn.addEventListener('click', function() {
+    overlay.classList.add('active');
+});
+
+signInBtn.addEventListener('click', function() {
+    overlay.classList.add('active');
+});
+
+closeBtn.addEventListener('click', function() {
+    overlay.classList.remove('active');
+});
