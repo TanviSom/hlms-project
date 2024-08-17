bafd6cf7955edcd650f6d449ef53cd2922599bd9
Tanvi Somvanshi <24somta@goodnews.vic.edu.au>
2024-08-17 17:23:11 +1000

    signup form created

--- static/dashboard.js
+++ static/dashboard.js
@@ -10,4 +10,59 @@ function openSidebar() {
     }
 }
 
-function
+function closeSidebar() {
+    if(sidebarOpen) {
+        sidebar.classList.remove("sidebar-responsive");
+        sidebarOpen= false;
+    }
+}
+
+var barChartOptions = {
+    series: [{
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+        show: false
+    },
+  },
+  colors: [
+    "#246dec",
+    "#cc3c43",
+    "#367952",
+    "#f5b74f",
+    "#4f35a1",
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,          
+      borderRadius: 4,
+      borderRadiusApplication: 'end',
+      horizontal: false,
+      columnWidth: '40%', 
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+
+  legend: {
+    show: false
+  },
+
+  xaxis: {
+    categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
+      'United States', 'China', 'Germany'
+    ],
+  },
+  yaxis: {
+    title: {
+        text:"Count"
+    }
+  }
+  };
+
+  var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+  barChart.render();
